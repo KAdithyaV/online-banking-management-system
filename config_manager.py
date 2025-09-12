46c2ebd9a1c723fdeab1c3170aff1371a4cbe3a7
TejaswiMargam <teja.margam@gmail.com>
2025-09-12 18:19:01 +0530

    Update config_manager.py

--- config_manager.py
+++ config_manager.py
@@ -8,49 +8,75 @@ class ConfigManager:
 
     def __init__(self):
         """If the config file doesn't exist, creates it with default values"""
-        if not os.path.exists(self.CONFIG_FILE):
-           
-            default_config = {
+        try:
+            if not os.path.exists(self.CONFIG_FILE):
+                default_config = {
                 "withdrawal_limit": 1000,
                 "minimum_balance": 100,
                 "max_transfer_amount": 5000 }
-           
-            with open(self.CONFIG_FILE, "w") as file:
-                json.dump(default_config, file)
+                with open(self.CONFIG_FILE, "w") as file:
+                    json.dump(default_config, file)
+
+        except Exception as e:
+            print(e)           
+                   
 
     def load_config(self):
         """Loads the configuration from the JSON file"""
-        with open(self.CONFIG_FILE, "r") as file:
-            return json.load(file)
+        try:
+             with open(self.CONFIG_FILE, "r") as file:
+                  return json.load(file)
+        except Exception as e:
+            print(e)           
+ 
 
     def save_config(self, config):
         """Saves the given configuration dictionary to the JSON file"""
-        with open(self.CONFIG_FILE, "w") as file:
-            json.dump(config, file)
+        try:
+            with open(self.CONFIG_FILE, "w") as file:
+                json.dump(config, file)
+
+        except Exception as e:
+            print(e)
+            
 
 
     def get_withdrawal_limit(self):
         """Returns the current withdrawal limit from the config"""
-        return self.load_config().get("withdrawal_limit")
+        try:
+            return self.load_config().get("withdrawal_limit")
+        except Exception as e:
+            print(e)  
 
 
     def get_minimum_balance(self):
         """Returns the current minimum balance requirement from the config"""
-        return self.load_config().get("minimum_balance")
+        try:
+             return self.load_config().get("minimum_balance")
+        except Exception as e:
+            print(e) 
+ 
 
 
     def get_max_transfer_amount(self):
         """Returns the current maximum transfer amount from the config"""
-        return self.load_config().get("max_transfer_amount")
+        try:
+            return self.load_config().get("max_transfer_amount")
+        except Exception as e:
+            print(e)
+            
+        
 
 
     def update_config(self, key, value):
         """Updates a specific key in the config file with a new value"""
-        config = self.load_config()
-        config[key] = value
-        self.save_config(config)
-
-
+        try:
+            config = self.load_config()
+            config[key] = value
+            self.save_config(config)
+        except Exception as e:
+            print(e)    
+        
 
 c = ConfigManager()
 print("Min balance:", c.get_minimum_balance())
